6e0a6cc0c26673d31db48f990eaf998a1b1b4d65
Krishnaprasad <skpkrishna25@gmail.com>
2026-09-22 22:48:56 +0530

    third commit

--- test_factorial.py
+++ test_factorial.py
@@ -1,11 +1,15 @@
+import pytest
 from factorial import factorial
 
-def test_factorial():
-    assert factorial(1)==1
+def test_factorial_of_zero():
+    assert factorial(0) == 1
 
-def test_factorial2():
+def test_factorial_of_five():
     assert factorial(5) == 120
 
+def test_factorial_of_ten():
+    assert factorial(10) == 3628800
 
-def test_negative_factorial3():
-    assert factorial(2) == 2
+def test_negative_number():
+    with pytest.raises(ValueError):
+        factorial(-1)
